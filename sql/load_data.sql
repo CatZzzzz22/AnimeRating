b8e875f99d13939a733c4fe51986238baededba1
罗睿子 <luoruizi@Graces-MacBook-Pro.local>
2025-07-06 19:01:04 -0400

    update load_data

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -37,7 +37,7 @@ IGNORE 1 LINES
 (uid, username, password, uname, gender, age, location, joinedDate);
 
 -- Load Ratings
-LOAD DATA LOCAL INFILE '../../data/sample_ratings.csv'
+LOAD DATA LOCAL INFILE '../../production_cleaned_data/rating.csv'
 INTO TABLE Rating
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
@@ -46,7 +46,7 @@ IGNORE 1 LINES
 (uid, aid, ratedDate, score);
 
 -- Load Watchlist
-LOAD DATA LOCAL INFILE '../../data/sample_watchlist.csv'
+LOAD DATA LOCAL INFILE '../../roduction_cleaned_data/watchlist.csv'
 INTO TABLE Watchlist
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
