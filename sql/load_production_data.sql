10456e33f152a122dbb1cb6e6caaef0cba656649
罗睿子 <luoruizi@Graces-MacBook-Pro.local>
2025-07-07 11:22:29 -0400

    fix typo

--- sql/load_production_data.sql
+++ sql/load_production_data.sql
@@ -7,7 +7,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"' 
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(aid, aname, score, synopsis, type, episodes, aired, imageURL, numRaing);
+(aid, aname, score, synopsis, type, episodes, aired, imageURL, numRating);
 
 -- Load Genre table
 LOAD DATA LOCAL INFILE '../../production_cleaned_data/genre.csv'
@@ -46,7 +46,7 @@ IGNORE 1 LINES
 (uid, aid, ratedDate, score);
 
 -- Load Watchlist
-LOAD DATA LOCAL INFILE '../../roduction_cleaned_data/watchlist.csv'
+LOAD DATA LOCAL INFILE '../../production_cleaned_data/watchlist.csv'
 INTO TABLE Watchlist
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"' 
