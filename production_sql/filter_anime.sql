eee87b2bdb35d81288225e525bca486e8169eac5
apple <apple@macbookpro.home>
2025-07-08 18:00:59 -0400

    search and filter test result

--- production_sql/filter_anime.sql
+++ production_sql/filter_anime.sql
@@ -1,13 +1,13 @@
-With PopulateAnime AS 
-SELECT A.*, GROUP_CONCAT(DISTINCT g.gname ORDER BY g.gname SEPARATOR ', ') as genre
+With PopulateAnime AS
+(SELECT A.*, GROUP_CONCAT(DISTINCT g.gname ORDER BY g.gname SEPARATOR ', ') as genre
 FROM Anime a join AnimeGenre ag on a.aid = ag.aid
     join Genre g on g.gid = ag.gid
 GROUP BY
     a.aid, a.aname, a.score, a.synopsis,
-    a.type, a.episodes, a.aired, a.imageURL, a.numRating
-
+    a.type, a.episodes, a.aired, a.imageURL, a.numRating)
+ 
 -- Match all anime where genres is romance
 SELECT aname, score, type, genre, episodes, aired, imageURL, synopsis
 FROM PopulateAnime
-WHERE LOWER(gname) = 'romance'
+WHERE LOWER(genre) LIKE "%romance%"
 LIMIT 6;
--- production_sql/search_anime.sql
+++ production_sql/search_anime.sql
@@ -1,13 +1,14 @@
 -- Match all anime where name contains 'ken'
-With PopulateAnime AS 
-SELECT A.*, GROUP_CONCAT(DISTINCT g.gname ORDER BY g.gname SEPARATOR ', ') as genre
+With PopulateAnime AS
+(SELECT A.*, GROUP_CONCAT(DISTINCT g.gname ORDER BY g.gname SEPARATOR ', ') as genre
 FROM Anime a join AnimeGenre ag on a.aid = ag.aid
     join Genre g on g.gid = ag.gid
 GROUP BY
     a.aid, a.aname, a.score, a.synopsis,
-    a.type, a.episodes, a.aired, a.imageURL, a.numRating
-
+    a.type, a.episodes, a.aired, a.imageURL, a.numRating)
+ 
 SELECT aname, score, type, genre, episodes, aired, imageURL, synopsis
 FROM PopulateAnime
 WHERE LOWER(aname) LIKE '%ken%'
 LIMIT 6;
+ 
