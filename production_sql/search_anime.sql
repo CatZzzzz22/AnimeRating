ec2a0dfc5a8d3103b9e11bffc85f14046c607333
cathy-wumengdie <wmd248655@outlook.com>
2025-07-08 17:22:55 -0400

    Filter and search test files (#50)

--- production_sql/search_anime.sql
+++ production_sql/search_anime.sql
@@ -1,5 +1,9 @@
 -- Match all anime where name contains 'ken'
-SELECT aname, score, type, gname, episode, aired, imageURL, synopsis
-FROM Anime
+WITH AnimeAggregate AS (Select A.*, G.gname
+                        FROM Anime A LEFT JOIN AnimeGenre AG ON A.aid = AG.aid
+                                     LEFT JOIN Genre G ON G.gid = AG.gid)
+
+SELECT aname, score, type, gname, episodes, aired, imageURL, synopsis
+FROM AnimeAggregate
 WHERE LOWER(aname) LIKE '%ken%'
 LIMIT 10;
