2fa039cd9a2db1d35c5ee759cfb0f3bd6adc731c
BlowWest <133542464+BlowWest@users.noreply.github.com>
2025-07-20 22:01:41 -0400

    Fixed fetch issue (#75)
    * watchlist running on production data
    
    * fixed fetch problem
    
    * fixed fetch problem

--- weebseek/frontend/src/helpers.tsx
+++ weebseek/frontend/src/helpers.tsx
@@ -2,7 +2,10 @@ const API_BASE = "http://localhost:5050";
 
 export async function apiFetch<T = any>(path: string, init: RequestInit = {}): Promise<T> {
   const url = `${API_BASE}${path}`;
-  const res = await fetch(url, init);
+  const res = await fetch(url, {
+    credentials: 'include',
+    ...init,
+  });
 
   if (!res.ok) {
     let errorMsg: string;
